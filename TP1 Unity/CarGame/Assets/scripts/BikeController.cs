7db6a93f2fbbd6628373f7b463e61420446e3bab
nudiasipvc <nudiasipvc@ipvc.pt>
2020-04-27 04:48:57 +0100

    TP1

---
+++ TP1 Unity/CarGame/Assets/scripts/BikeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class BikeController : MonoBehaviour
+{
+    public float spreed = 1500f;
+    public float rotationSpeed = 15f;
+         
+
+    public WheelJoint2D backWheel;
+    public WheelJoint2D frontWheel;
+    public Rigidbody2D rb;
+
+
+    private float movement = 0f;
+    private float rotation = 0f;
+
+
+    void Update ()
+    {
+        movement = -Input.GetAxisRaw("Vertical") * spreed;
+        rotation = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate ()
+    {
+        if(movement == 0f)
+        {
+            backWheel.useMotor = false;
+            frontWheel.useMotor = false;
+
+        }
+        else
+        {
+            backWheel.useMotor = true;
+            frontWheel.useMotor = true;
+
+            JointMotor2D motor = new JointMotor2D { motorSpeed = movement, maxMotorTorque = 100000f };
+            backWheel.motor = motor;
+            frontWheel.motor = motor;
+
+        }
+
+        rb.AddTorque(-rotation * rotationSpeed * Time.fixedDeltaTime);
+
+    }
+
+}
---
+++ TP1 Unity/CarGame/Assets/scripts/Goal.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D colInfo)
+    {
+        if(colInfo.CompareTag("Player"))
+        {
+            Debug.Log("PARABENS, GANHOU!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
