c44f7fc8d0daaf72837cee2c85dffef2eb686a8c
nudiasipvc <nudiasipvc@ipvc.pt>
2020-04-27 14:17:19 +0100

    Trab11

---
+++ TP1 Unity/CarGame/Assets/scripts/Goal1.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal1 : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D colInfo)
+    {
+        if(colInfo.CompareTag("Player"))
+        {
+            Debug.Log("PARABENS, GANHOU!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
